aabdf8bd448b45b038a0905df721c5f818f920ac
Zach Byers <byerszach@gmail.com>
2018-03-20 18:55:19 -0600

    all pins placed

--- src/components/USMap.js
+++ src/components/USMap.js
@@ -6,28 +6,54 @@ class USMap extends React.Component {
     return(
       <Segment basic>
         <Image
+          id='map'
           src='http://flutterby.us/wp-content/uploads/2017/11/14-map-the-usa-hd-wallpapers-of-usa-map.jpg'
           style={styles.map}
         />
         <Image
+          id='highpoint'
           src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
           style={styles.highpoint}
         />
         <Image
+          id='mtairy'
           src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
           style={styles.mtairy}
         />
         <Image
+          id='troy'
+          src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
+          style={styles.troy}
+        />
+        <Image
+          id='belmont'
           src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
           style={styles.belmont}
         />
         <Image
+          id='hickory'
           src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
           style={styles.hickory}
         />
         <Image
+          id='jefferson'
           src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
-          style={styles.troy}
+          style={styles.jefferson}
+        />
+        <Image
+          id='phoenix'
+          src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
+          style={styles.phoenix}
+        />
+        <Image
+          id='gilbert'
+          src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
+          style={styles.gilbert}
+        />
+        <Image
+          id='louisville'
+          src='http://www.clker.com/cliparts/N/g/p/e/K/Q/red-pin-no-shadow-hi.png'
+          style={styles.louisville}
         />
       </Segment>
     )
@@ -68,6 +94,30 @@ const styles = {
     height: '5vh',
     top: '43.5vh',
     left: '101vh',
+  },
+  jefferson: {
+    position: 'absolute',
+    height: '5vh',
+    top: '48vh',
+    left: '97.5vh',
+  },
+  gilbert: {
+    position: 'absolute',
+    height: '5vh',
+    top: '49vh',
+    left: '29vh',
+  },
+  louisville: {
+    position: 'absolute',
+    height: '5vh',
+    top: '32vh',
+    left: '47vh',
+  },
+  phoenix: {
+    position: 'absolute',
+    height: '5vh',
+    top: '49vh',
+    left: '28vh',
   },
 }
 
